6e1a07d39fd3387dbc540a6ee09936394d366906
Maurus Cuelenaere <mcuelenaere@gmail.com>
2021-02-18 21:15:43 +0100

    blpapi-sys: dynamically generate bindings at build time

--- blpapi-sys/build.rs
+++ blpapi-sys/build.rs
@@ -1,57 +1,57 @@
-use std::fs;
+use std::env;
 use std::path::PathBuf;
 
 const ENV_WARNING: &'static str = r#"Error while building blpapi-sys.
 
-    Cannot find 'BLPAPI_LIB' environment variable.
+    Cannot find 'BLPAPI_ROOT' environment variable.
 
     You can download blpapi binaries from bloomberg at:
     https://www.bloomberg.com/professional/support/api-library/
 
-    Once extracted, the BLPAPI_LIB environment variable should point to the
-    corresponding lib dir:
-
-    - windows: <EXTRACT_PATH>\lib
-    - linux: <EXTRACT_PATH>/Linux"
+    Once extracted, the BLPAPI_ROOT environment variable should point to the
+    directory containing the extracted package.
 "#;
 
 fn main() {
-    // Use the bundled libs in `../vendor`
-    let lib_dir = if cfg!(feature = "bundled") {
-        let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        dir.pop();
-        dir.push("vendor");
+    let blpapi_root_dir = PathBuf::from(env::var("BLPAPI_ROOT").expect(ENV_WARNING));
 
-        // Select the correct folder for this OS based on the folder suffix.
-        // E.g.
-        //  Windows => ../vendor/xxx-windows/lib/
-        //  Linux => ../vendor/xxx-linux/Linux/
-        for entry in fs::read_dir(dir.as_path()).expect("Failed to read `vendor/` dir") {
-            let entry = entry.expect("Failed to read entry in `vendor/` dir");
-            let path = entry.path();
-            if path.is_dir() {
-                let dirname = path.file_name().unwrap_or_default().to_string_lossy();
-                if cfg!(target_os = "windows") && dirname.ends_with("windows") {
-                    dir.push(path);
-                    dir.push("lib");
-                    break;
-                } else if cfg!(target_os = "linux") && dirname.ends_with("linux") {
-                    dir.push(path);
-                    dir.push("Linux");
-                    break;
-                } else if cfg!(target_os = "macos") && dirname.ends_with("macos") {
-                    dir.push(path);
-                    dir.push("Darwin");
-                }
-            }
+    let lib_dir = {
+        let mut dir = blpapi_root_dir.clone();
+
+        if cfg!(target_os = "windows") {
+            dir.push("lib");
+        } else if cfg!(target_os = "linux") {
+            dir.push("Linux");
+        } else if cfg!(target_os = "macos") {
+            dir.push("Darwin");
         }
+
         dir.into_os_string().into_string().unwrap()
-    } else {
-        //TODO: use pkg-config to search in system lib dirs instead of
-        //only relying on env variable
-        std::env::var("BLPAPI_LIB").expect(ENV_WARNING)
     };
 
     println!("cargo:rustc-link-search={}", lib_dir);
     println!("cargo:rustc-link-lib=blpapi3_64");
+
+    let include_dir = {
+        let mut dir = blpapi_root_dir.clone();
+        dir.push("include");
+        dir.into_os_string().into_string().unwrap()
+    };
+
+    // Dynamically build bindings.rs based on wrapper.h
+    println!("cargo:rerun-if-changed=wrapper.h");
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg(format!("-I{}", include_dir))
+        .size_t_is_usize(true)
+        .derive_default(true)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- blpapi-sys/src/lib.rs
+++ blpapi-sys/src/lib.rs
@@ -1,7 +1,8 @@
-#[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
-mod bindings;
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-pub use bindings::*;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[test]
 fn test_historical() {
